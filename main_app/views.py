d654a7accbabdd8a0d6288e48fdd260d38febca7
ed <cabrale234@gmail.com>
2022-01-25 00:22:11 -0500

    added delete quantity adjustment functionality for cart component

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import UpdateView, DeleteView
 from .models import Product, SubCart, Cart
+from .forms import QuantityForm
 from django.http import HttpResponse
 # Create your views here.
 
@@ -20,8 +22,28 @@ def products_detail(request, product_id):
 
 def cart_index(request):
     carts = SubCart.objects.all()
-    return render(request, 'cart/index.html', {'carts': carts})
+    quantity_form = QuantityForm()
+    return render(request, 'cart/index.html', {'carts': carts , 'quantity_form': quantity_form})
 
 def cart_detail(request, cart_id):
   cart = SubCart.objects.get(id=cart_id)
   return render(request, 'cart/detail.html', {'cart': cart})
+
+# def add_quantity(request, cart_id):
+#   print('this string!!!!!!!!!!!')
+#   print(request)
+#   form = QuantityForm(request)
+#   print(form)
+#   if form.is_valid():
+#     add_quantity = form.save(commit=False)
+#     cart.quantity = add_quantity
+#     cart.save()
+#   return redirect('cart_detail', cart_id=cart_id)
+
+class SubCartUpdate(UpdateView):
+  model = SubCart
+  fields = ['quantity']
+
+class SubCartDelete(DeleteView):
+  model = SubCart
+  success_url = '/cart/'
